eb03ff96ad48c10f3ebbc46f3f5636dfec44fbe3
Luke Murphy <lukewm@riseup.net>
2020-06-30 16:02:55 +0200

    Add __init__ setup, pytest-trio and split up tests

--- simple_message_channels/smc.py
+++ simple_message_channels/smc.py
@@ -2,9 +2,36 @@
 
 __all__ = ["SimpleMessageChannel"]
 
+from typing import List, Optional
 
+import attr
+
+
+@attr.s(auto_attribs=True)
 class SimpleMessageChannel:
     """A simple message channel."""
+
+    message: Optional[bytes] = None
+    ptr: int = 0
+    varint: int = 0
+    factor: int = 1
+    length: int = 0
+    header: int = 0
+    state: int = 0
+    consumed: int = 0
+    max_size: int = 8 * 1024 * 1024
+    types: List = attr.Factory(list)
+
+    receiving: bool = False
+    destroyed: bool = False
+    error: Optional[Exception] = None
+
+    # TODO(decentral1se): need to lookup type of what is being passed in
+    # context: ???
+
+    # TODO(decentral1se): allow to override instead of callback interface!?
+    # onmissing: ???
+    # onmessage: ???
 
     async def send(self, channel: int, type: int, message: bytes) -> bytes:
         pass
--- test/test_smc.py
+++ test/test_smc.py
@@ -1,10 +1,17 @@
-async def test_send_recv(smc):
+async def test_send_produces_bytes(smc):
     channel, type, message = 0, 0, b"abc"
 
     payload = await smc.send(channel, type, message)
     assert isinstance(payload, bytes)
 
-    async with smc.recv(payload) as response:
-        assert response.channel == channel
-        assert response.type == type
-        assert response.message == message
+
+# async def test_recv_context_manager(smc):
+#     channel, type, message = 0, 0, b"abc"
+
+#     payload = await smc.send(channel, type, message)
+#     assert isinstance(payload, bytes)
+
+#     async with smc.recv(payload) as response:
+#         assert response.channel == channel
+#         assert response.type == type
+#         assert response.message == message
